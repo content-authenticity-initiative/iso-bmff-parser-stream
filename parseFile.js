9af2e8b82fd1df2bd883435647f87f565cead9b3
Leonard Rosenthol <leonardr@lazerware.com>
2021-09-05 14:52:19 -0400

    add try/catch for errors in parseFile

--- parseFile.js
+++ parseFile.js
@@ -6,18 +6,22 @@ var myArgs = process.argv.slice(2);
 // console.log('myArgs: ', myArgs);
 var fName = myArgs[0];
 
-var fStream = fs.createReadStream(fName, {
-	flags: 'r',
-	encoding: null,
-	fd: null,
-	mode: 0666,
-	autoClose: true
-});
+try {
+	var fStream = fs.createReadStream(fName, {
+		flags: 'r',
+		encoding: null,
+		fd: null,
+		mode: 0666,
+		autoClose: true
+	});
 
-var unboxing = new bmffParser(function (err, data) {
-	// pretty print the output at 4 spaces (null, 4)
-	console.log(JSON.stringify(data, null, 4));
-});
+	var unboxing = new bmffParser(function (err, data) {
+		// pretty print the output at 4 spaces (null, 4)
+		console.log(JSON.stringify(data, null, 4));
+	});
 
-fStream
-	.pipe(unboxing);
+	fStream
+		.pipe(unboxing);
+} catch (err) {
+	console.error(err)
+}
